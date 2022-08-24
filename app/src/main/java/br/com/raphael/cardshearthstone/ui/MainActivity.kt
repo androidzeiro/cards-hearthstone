df7b1f92aac291e74ddebd67db3b5c2bfa2efea4
Raphael Amorim <raphael_amorim@outlook.com>
2022-08-24 00:17:08 -0300

    feat: Implementado a BaseActivity na MainActivity

---
+++ app/src/main/java/br/com/raphael/cardshearthstone/ui/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.raphael.cardshearthstone.ui
+
+import br.com.raphael.cardshearthstone.databinding.ActivityMainBinding
+import br.com.raphael.cardshearthstone.ui.base.BaseActivity
+
+class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::inflate)
