051a6d80a5794df48c92dbb2ca831dfe3e0b9e7a
Raphael Amorim <raphael_amorim@outlook.com>
2022-08-24 00:07:38 -0300

    feat: Adicionado classes base para facilitar a criação do View Binding na Activity e nos Fragments

---
+++ app/src/main/java/br/com/raphael/cardshearthstone/ui/base/BaseActivity.kt
@@ -0,0 +1,25 @@
+package br.com.raphael.cardshearthstone.ui.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import androidx.appcompat.app.AppCompatActivity
+import androidx.viewbinding.ViewBinding
+
+open class BaseActivity<T: ViewBinding>(
+    private val inflateMethod: (LayoutInflater) -> T
+) : AppCompatActivity() {
+
+    private var _binding: T? = null
+    val binding: T get() = _binding!!
+
+    open fun T.initialize(savedInstanceState: Bundle?) {}
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        _binding = inflateMethod.invoke(layoutInflater).apply {
+            setContentView(root)
+        }
+        binding.initialize(savedInstanceState)
+    }
+}
