cc9e58f69a61e544185f66f4a6415e03e5431d0c
Raphael Amorim <raphael_amorim@outlook.com>
2022-08-24 22:12:37 -0300

    feat: Adicionado classe Card

---
+++ app/src/main/java/br/com/raphael/cardshearthstone/data/model/Card.kt
@@ -0,0 +1,16 @@
+package br.com.raphael.cardshearthstone.data.model
+
+data class Card(
+    val cardId: String,
+    val img: String,
+    val name: String,
+    val flavor: String,
+    val text: String,
+    val cardSet: String,
+    val type: String,
+    val faction: String = "Sem facção",
+    val rarity: String,
+    val attack: Int,
+    val cost: Int,
+    val health: Int
+)
