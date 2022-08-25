86f461023131e6b76e3ce56ee4f3a28dbfb390f3
Raphael Amorim <raphael_amorim@outlook.com>
2022-08-24 22:02:55 -0300

    feat: Adicionado ApiModule

---
+++ app/src/main/java/br/com/raphael/cardshearthstone/di/ApiModule.kt
@@ -0,0 +1,80 @@
+package br.com.raphael.cardshearthstone.di
+
+import br.com.raphael.cardshearthstone.BuildConfig
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class ApiModule {
+
+    @Singleton
+    @Provides
+    fun provideHttpLoggingInterceptor(): HttpLoggingInterceptor {
+        val httpLoggingInterceptor = HttpLoggingInterceptor()
+        httpLoggingInterceptor.level = if (BuildConfig.DEBUG) {
+            HttpLoggingInterceptor.Level.BODY
+        } else {
+            HttpLoggingInterceptor.Level.NONE
+        }
+        return httpLoggingInterceptor
+    }
+
+    @Singleton
+    @Provides
+    fun provideNetworkInterceptor(): Interceptor {
+        return Interceptor {chain ->
+            val originalRequest = chain.request()
+            val newRequest = originalRequest.newBuilder().apply {
+                addHeader("X-RapidAPI-Key", BuildConfig.API_KEY)
+                addHeader("X-RapidAPI-Host", BuildConfig.API_HOST)
+            }.build()
+            chain.proceed(newRequest)
+        }
+    }
+
+    @Singleton
+    @Provides
+    fun provideOkHttpClient(
+        networkInterceptor: Interceptor,
+        loggingInterceptor: HttpLoggingInterceptor
+    ): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor(networkInterceptor)
+            .addInterceptor(loggingInterceptor)
+            .connectTimeout(30, TimeUnit.SECONDS)
+            .writeTimeout(30, TimeUnit.SECONDS)
+            .readTimeout(30, TimeUnit.SECONDS)
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideMoshi(): Moshi {
+        return Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(
+        okHttpClient: OkHttpClient,
+        moshi: Moshi,
+    ): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BuildConfig.API_URL)
+            .client(okHttpClient)
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .build()
+    }
+}
