b037f3afc94ea7836b3ebe0c08f270aed40bb043
Raphael Amorim <raphael_amorim@outlook.com>
2022-08-24 23:08:23 -0300

    feat: Adicionado @AndroidEntryPoint na MainActivity e no CardsFragment

--- app/src/main/java/br/com/raphael/cardshearthstone/ui/MainActivity.kt
+++ app/src/main/java/br/com/raphael/cardshearthstone/ui/MainActivity.kt
@@ -2,5 +2,7 @@ package br.com.raphael.cardshearthstone.ui
 
 import br.com.raphael.cardshearthstone.databinding.ActivityMainBinding
 import br.com.raphael.cardshearthstone.ui.base.BaseActivity
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::inflate)
--- app/src/main/java/br/com/raphael/cardshearthstone/ui/view/cards/CardsFragment.kt
+++ app/src/main/java/br/com/raphael/cardshearthstone/ui/view/cards/CardsFragment.kt
@@ -2,5 +2,7 @@ package br.com.raphael.cardshearthstone.ui.view.cards
 
 import br.com.raphael.cardshearthstone.databinding.FragmentCardsBinding
 import br.com.raphael.cardshearthstone.ui.base.BaseFragment
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class CardsFragment : BaseFragment<FragmentCardsBinding>(FragmentCardsBinding::inflate)
