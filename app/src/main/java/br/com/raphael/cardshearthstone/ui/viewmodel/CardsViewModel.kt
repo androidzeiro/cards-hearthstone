fb130b26be4f623dc03cac0ef5cb3e2c0ff5184b
Raphael Amorim <raphael_amorim@outlook.com>
2022-08-24 23:19:50 -0300

    feat: Adicionado CardsViewModel

---
+++ app/src/main/java/br/com/raphael/cardshearthstone/ui/viewmodel/CardsViewModel.kt
@@ -0,0 +1,40 @@
+package br.com.raphael.cardshearthstone.ui.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import br.com.raphael.cardshearthstone.data.repository.CardsRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import br.com.raphael.cardshearthstone.data.model.dto.Result
+import br.com.raphael.cardshearthstone.data.model.dto.State
+import br.com.raphael.cardshearthstone.data.model.response.CardResponse
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class CardsViewModel @Inject constructor(
+    private val cardsRepository: CardsRepository
+) : ViewModel() {
+
+    private val _cardsState = MutableLiveData<State<List<CardResponse>>>()
+    val cardsState: LiveData<State<List<CardResponse>>> = _cardsState
+
+    fun getCards() {
+        viewModelScope.launch {
+            cardsRepository.getCards().collect { result ->
+                when (result) {
+                    is Result.Success -> {
+                        _cardsState.postValue(State.Success(result.value))
+                    }
+                    is Result.Error -> {
+                        _cardsState.postValue(State.Error(result.message))
+                    }
+                    is Result.NetworkError -> {
+                        _cardsState.postValue(State.Error(result.message))
+                    }
+                }
+            }
+        }
+    }
+}
