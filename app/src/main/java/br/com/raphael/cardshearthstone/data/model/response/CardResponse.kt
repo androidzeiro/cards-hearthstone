863ef2d2b0e9292977db77481d9a847f5cfed48e
Raphael Amorim <raphael_amorim@outlook.com>
2022-08-24 23:25:55 -0300

    fix: Refatorado a classe CardResponse para poder receber valores nulos

--- app/src/main/java/br/com/raphael/cardshearthstone/data/model/response/CardResponse.kt
+++ app/src/main/java/br/com/raphael/cardshearthstone/data/model/response/CardResponse.kt
@@ -2,15 +2,15 @@ package br.com.raphael.cardshearthstone.data.model.response
 
 data class CardResponse(
     val cardId: String,
-    val img: String,
-    val name: String,
-    val flavor: String,
-    val text: String,
-    val cardSet: String,
-    val type: String,
-    val faction: String = "Sem facção",
-    val rarity: String,
-    val attack: Int,
-    val cost: Int,
-    val health: Int
+    val img: String?,
+    val name: String?,
+    val flavor: String?,
+    val text: String?,
+    val cardSet: String?,
+    val type: String?,
+    val faction: String?,
+    val rarity: String?,
+    val attack: Int?,
+    val cost: Int?,
+    val health: Int?
 )
