d59090be64f9048deeafe7261f3b328b0f57893a
Raphael Amorim <raphael_amorim@outlook.com>
2022-08-24 22:50:32 -0300

    feat: Adicionado o object FlowApiCall para facilitar o tratamento dos requests

---
+++ app/src/main/java/br/com/raphael/cardshearthstone/data/remote/FlowApiCall.kt
@@ -0,0 +1,63 @@
+package br.com.raphael.cardshearthstone.data.remote
+
+import android.content.Context
+import br.com.raphael.cardshearthstone.R
+import br.com.raphael.cardshearthstone.data.model.ErrorResponse
+import br.com.raphael.cardshearthstone.di.ApiModule
+import br.com.raphael.cardshearthstone.util.extension.isInternetAvailable
+import kotlinx.coroutines.flow.FlowCollector
+import okhttp3.ResponseBody
+import retrofit2.Response
+import timber.log.Timber
+
+object FlowApiCall {
+    private const val TAG_ERROR_BODY = "convertErrorBody()"
+    private const val TAG_RESULT = "getResult()"
+
+    suspend fun <T> getResult(
+        context: Context,
+        flowCollector: FlowCollector<Result<T>>,
+        request: suspend () -> Response<T>
+    ) {
+        if (context.isInternetAvailable()) {
+            try {
+                val response = request()
+                val body = response.body()
+                if (response.isSuccessful && body != null) {
+                    flowCollector.emit(Result.Success(body, response.code()))
+                } else {
+                    val errorResponse = convertErrorBody(response.errorBody())
+                    if (errorResponse != null) {
+                        flowCollector.emit(Result.Error(errorResponse.message, response.code()))
+                    } else {
+                        val errorMessage = when (response.code()) {
+                            401 -> context.getString(R.string.error_unauthorized_message)
+                            404 -> context.getString(R.string.error_not_found)
+                            else -> context.getString(R.string.error_generic_message)
+                        }
+                        flowCollector.emit(Result.Error(errorMessage, response.code()))
+                    }
+                }
+            } catch (exception: Exception) {
+                Timber.tag(TAG_RESULT).e(exception)
+                flowCollector.emit(Result.Error(context.getString(R.string.error_generic_message), 0))
+            }
+        } else {
+            flowCollector.emit(Result.NetworkError(context.getString(R.string.error_network)))
+        }
+    }
+
+    private fun convertErrorBody(errorBody: ResponseBody?): ErrorResponse? {
+        var errorResponse: ErrorResponse? = null
+        try {
+            errorResponse = errorBody?.source()?.let {
+                val jsonAdapter = ApiModule.provideMoshi().adapter(ErrorResponse::class.java)
+                val message = it.readString(Charsets.UTF_8)
+                jsonAdapter.fromJson(message)
+            }
+        } catch (e: Exception) {
+            Timber.tag(TAG_ERROR_BODY).e(e)
+        }
+        return errorResponse
+    }
+}
--- app/src/main/java/br/com/raphael/cardshearthstone/di/ApiModule.kt
+++ app/src/main/java/br/com/raphael/cardshearthstone/di/ApiModule.kt
@@ -18,7 +18,7 @@ import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-class ApiModule {
+object ApiModule {
 
     @Singleton
     @Provides
