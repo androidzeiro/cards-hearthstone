9ceb264557e7c2ad9d456848d3b87109349e77d4
Raphael Amorim <raphael_amorim@outlook.com>
2022-08-24 22:26:39 -0300

    feat: Implementado Timber no Application

--- app/src/main/java/br/com/raphael/cardshearthstone/application/CardsHeartstoneApplication.kt
+++ app/src/main/java/br/com/raphael/cardshearthstone/application/CardsHeartstoneApplication.kt
@@ -1,7 +1,17 @@
 package br.com.raphael.cardshearthstone.application
 
 import android.app.Application
+import br.com.raphael.cardshearthstone.BuildConfig
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
-class CardsHeartstoneApplication : Application()
+class CardsHeartstoneApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+    }
+}
