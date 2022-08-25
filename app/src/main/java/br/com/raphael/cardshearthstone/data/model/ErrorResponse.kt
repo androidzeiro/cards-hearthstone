d81fa7d92e91da1ea6c976a88171e2168eb6ff2f
Raphael Amorim <raphael_amorim@outlook.com>
2022-08-24 22:50:06 -0300

    feat: Adicionado classes Result e ErrorResponse

---
+++ app/src/main/java/br/com/raphael/cardshearthstone/data/model/ErrorResponse.kt
@@ -0,0 +1,5 @@
+package br.com.raphael.cardshearthstone.data.model
+
+data class ErrorResponse(
+    val message: String
+)
---
+++ app/src/main/java/br/com/raphael/cardshearthstone/data/remote/Result.kt
@@ -0,0 +1,7 @@
+package br.com.raphael.cardshearthstone.data.remote
+
+sealed class Result<out T> {
+    data class Success<out T>(val value: T, val statusCode: Int) : Result<T>()
+    data class Error(val message: String, val statusCode: Int) : Result<Nothing>()
+    data class NetworkError(val message: String) : Result<Nothing>()
+}
